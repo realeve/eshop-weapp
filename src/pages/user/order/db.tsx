a04e5d9158a11fb851ae1ac04b32ebc0981147a2
realeve <realeve@qq.com>
2020-02-21 17:33:34 +0800

    我的订单数据对接调试

--- src/pages/user/order/db.tsx
+++ src/pages/user/order/db.tsx
@@ -2,6 +2,7 @@ import { axios } from "@/utils/axios";
 import { ORDER, API } from "@/utils/api";
 import { CLIENT_TYPE } from "@/utils/setting";
 import success from "@/components/Toast/success";
+import * as R from "ramda";
 
 export const orderStateList: {
   name: string;
@@ -112,3 +113,357 @@ export const removeBrowseAll = () => {
     data: { clientType: CLIENT_TYPE.web }
   });
 };
+
+/**
+*  @params closed   已取消 
+   @params needPay  待付款
+   @params payed    待发货
+   @params sending  待收货
+   @params complete 交易成功，待评价
+   @params commented 已评价
+   @params appendCommented 已追评
+ */
+export enum EOrderStatus {
+  closed = 0, // 已取消
+  needPay = 10, // 待付款
+  payed = 20,
+  sending = 30,
+  complete = 40,
+  commented = 40 + 1, // 状态为40，评价标志为1，两者相加
+  appendCommented = 40 + 1 + 1 // 已追评
+}
+
+export const getStatusByName: (name: string) => number = (name: string) => {
+  let res = 0;
+  switch (name) {
+    case "待付款":
+      res = EOrderStatus.needPay;
+      break;
+    case "待发货":
+      res = EOrderStatus.payed;
+      break;
+    case "待收货":
+      res = EOrderStatus.sending;
+      break;
+    case "待评价":
+      res = EOrderStatus.complete;
+      break;
+    case "已评价":
+      res = EOrderStatus.commented;
+      break;
+    case "已追评":
+      res = EOrderStatus.appendCommented;
+      break;
+    case "已取消":
+      res = EOrderStatus.closed;
+      break;
+    default:
+    case "全部订单":
+      res = -1;
+      break;
+  }
+  return res;
+};
+
+export const getNameByStatus: (status: number) => string = status => {
+  let res = "";
+
+  switch (Number(status)) {
+    case EOrderStatus.needPay:
+      res = "待付款";
+      break;
+    case EOrderStatus.payed:
+      res = "待发货";
+      break;
+    case EOrderStatus.sending:
+      res = "待收货";
+      break;
+    case EOrderStatus.complete:
+    case EOrderStatus.commented:
+    case EOrderStatus.appendCommented:
+      res = "交易成功";
+      break;
+    case EOrderStatus.closed:
+    default:
+      res = "交易关闭";
+      break;
+  }
+  return res;
+};
+
+// 订单状态
+export const orderStatus = getNameByStatus;
+
+export let convertOrderData = (data: IOrderItem[] | null) => {
+  if (R.isNil(data)) {
+    return [];
+  }
+  let res = data.map(orderItem =>
+    orderItem.ordersVoList.map(item => ({
+      payId: item.payId,
+      shop: item.storeName,
+      shopId: item.storeId,
+      orderId: item.ordersId,
+      orderSN: item.ordersSn,
+      orderTime: item.createTime,
+      autoCancelTime: item.autoCancelTime, // 自动取消时间
+      autoReceiveTime: item.autoReceiveTime, // 自动收货时间
+      ordersRefundState: item.ordersRefundState, // 退款状态
+
+      payAmount: orderItem.ordersOnlineDiffAmount, // 订单还须在线支付金额金额(扣除已经使用站内余额支付的部分)，当该金额>0时，出现支付按钮
+
+      isVirtual: item.isVirtual, // 虚拟商品
+
+      shipSn: item.shipSn,
+      shipCode: item.shipCode,
+
+      // 发货时间
+      sendTime: item.sendTime,
+
+      type: item.paymentClientType, // 支付方式 'WEB'
+      price: item.ordersAmount,
+      express: item.freightAmount,
+
+      // 此处服务端返回数据中已完成订单，无论是否评价，状态均为40，此处处理当已完成时需加上评价状态evaluationState，1为已评价
+      // 如果已追评,evaluationAppendState显示为1，需再次做区分
+      status:
+        item.ordersState != EOrderStatus.complete
+          ? item.ordersState
+          : item.ordersState +
+            item.evaluationState +
+            item.evaluationAppendState,
+
+      statusName: item.ordersStateName,
+
+      invoice: {
+        // TODO 接口暂无数据
+      },
+      goods: item.ordersGoodsVoList.map(voList => ({
+        spuid: voList.commonId,
+        goodsId: voList.goodsId,
+        ordersGoodsId: voList.ordersGoodsId,
+        title: voList.goodsName,
+        url: voList.imageSrc,
+        type: voList.goodsFullSpecs,
+        price: voList.goodsPrice,
+        count: voList.buyNum
+      })),
+      address: {
+        name: item.receiverName,
+        phone: item.receiverPhone,
+        detail: item.receiverAreaInfo,
+        address: item.receiverAddress
+      }
+    }))
+  );
+  return R.flatten(res);
+};
+
+export interface IOrderItem {
+  isChain: number;
+  isGroup: number;
+  isVirtual: number;
+  ordersOnlineDiffAmount: number;
+  ordersVoList: IOrdersVoList[];
+  payId: number;
+  paySn: number;
+}
+
+interface IOrdersVoList {
+  ordersGoodsVoList: IOrdersGoodsVoList[];
+  autoCancelSecond: number;
+  autoCancelTime: string;
+  autoCancelTimeRemain: number;
+  autoReceiveTime: any;
+  autoReceiveTimeRemain: number;
+  chainId: number;
+  chainName: string;
+  chainOrdersType: number;
+  createTime: string;
+  delayReceiveState: number;
+  deleteState: number;
+  evaluationAppendState: number;
+  evaluationState: number;
+  evaluationTime: any;
+  finishTime: any;
+  freightAmount: number;
+  goId: number;
+  goState: number;
+  goodsName: string;
+  groupId: number;
+  groupNeedNum: number;
+  groupRemainNum: number;
+  imIsOnline: number;
+  isShowRefundOrdersList: number;
+  isVirtual: number;
+  lockState: number;
+  memberId: number;
+  memberName: string;
+  ordersAmount: number;
+  ordersBookList: any[];
+  ordersGiftVoList: any[];
+  ordersId: number;
+  ordersRefundId: number;
+  ordersRefundState: number;
+  ordersRefundType: number;
+  ordersSn: number;
+  ordersState: number;
+  ordersStateName: string;
+  ordersType: number;
+  ordersTypeName: string;
+  ordersTypeTag: number;
+  payId: number;
+  paymentClientType: string;
+  paymentCode: string;
+
+  paymentName: string;
+  paymentTime: any;
+  paymentTypeCode: string;
+  predepositAmount: number;
+  receiverAddress: string;
+  receiverAreaInfo: string;
+  receiverMessage: string;
+  receiverName: string;
+
+  receiverPhone: string;
+  receiverPhoneEncrypt: string;
+  refundAmount: any;
+  refundItemVo: any;
+  refundState: number;
+  searchOrdersState: number;
+  sellerId: number;
+  sellerName: string;
+
+  sendTime: string;
+
+  shipCode: string;
+  shipName: string;
+  shipSn: string;
+  shipUrl: any;
+  showAutoCancelSecond: number;
+  showEvaluation: number;
+  showEvaluationAppend: number;
+  showMemberApplyForSale: number;
+  showMemberBuyAgain: number;
+  showMemberCancel: number;
+  showMemberComplain: number;
+  showMemberDelayReceive: number;
+  showMemberDelete: number;
+  showMemberPay: number;
+  showMemberReceive: number;
+  showMemberRecoveryDelete: number;
+  showMemberRefundAll: number;
+  showMemberVirtualRefund: number;
+  showMemberVirtualRefundWaiting: number;
+  showRefundWaiting: number;
+  showShipSearch: number;
+  showViewRefundAll: number;
+  storeAddress: any;
+  storeId: number;
+  storeName: string;
+  storePhone: any;
+}
+
+interface IOrdersGoodsVoList {
+  allowVirtualRefund: number;
+  buyNum: number;
+  commonId: number;
+  complainId: number;
+  goodsContractVoList: any[];
+  goodsFullSpecs: string;
+  goodsId: number;
+  goodsImage: string;
+  goodsName: string;
+  goodsPayAmount: number;
+  goodsPrice: number;
+  goodsServicesList: any[];
+  imageSrc: string;
+  isExistLock: number;
+  ordersGoodsId: number;
+  ordersGoodsRefundId: number;
+  ordersGoodsRefundState: number;
+  ordersGoodsRefundType: number;
+  ordersId: number;
+  promotionTitle: string;
+  refundAmount: number;
+  refundId: number;
+  refundSn: number;
+  refundType: number;
+  showMemberComplain: number;
+  showRefund: number;
+  showRefundInfo: number;
+  showViewRefund: number;
+  showViewReturn: number;
+  tagTitle: string;
+  unitName: string;
+  virtualOrdersGoodsCodeList: any[];
+  virtualRefundApplyed: number;
+}
+
+export enum EServiceStatus {
+  processing, //商家处理中
+  write, //填写发货单
+  confirm, //商家确认收获中
+  returnedFail, //退货退款失败
+  returnedSuccess, //退货退款成功
+  refundFail, //退款失败
+  refundSuccess //退款成功
+}
+
+export const getStatusName: (name: string) => number = (name: string) => {
+  let res = 0;
+  switch (name) {
+    case "退货退款":
+      res = EServiceStatus.processing;
+      break;
+    case "退款":
+      res = EServiceStatus.write;
+      break;
+    default:
+    case "全部售后":
+      res = -1;
+      break;
+  }
+  return res;
+};
+
+export const getNameStatus: (status: number) => string = status => {
+  let res = "";
+  switch (Number(status)) {
+    case EServiceStatus.processing:
+      res = "取消申请";
+      break;
+    case EServiceStatus.write:
+      res = "填写发货单";
+      break;
+    case EServiceStatus.confirm:
+      res = "服务详情";
+      break;
+    case EServiceStatus.returnedFail:
+      res = "服务详情";
+      break;
+    case EServiceStatus.returnedSuccess:
+      res = "服务详情";
+      break;
+    case EServiceStatus.refundFail:
+      res = "服务详情";
+      break;
+    case EServiceStatus.refundSuccess:
+      res = "服务详情";
+      break;
+    default:
+      res = "已关闭";
+      break;
+  }
+  return res;
+};
+
+export const serviceStatus = [
+  "商家申请处理中",
+  "填写发货单",
+  "商家确认收货中",
+  "退货退款失败",
+  "退货退款成功",
+  "退款失败",
+  "退款成功"
+];
--- src/pages/user/order/index.tsx
+++ src/pages/user/order/index.tsx
@@ -3,29 +3,170 @@ import { View, Image, Text } from "@tarojs/components";
 import "./index.scss";
 import Tab from "@/pages/search/tab/";
 import * as db from "./db";
-import ListView, { LazyBlock } from "taro-listview";
+import ListView from "taro-listview";
 import useSetState from "@/components/hooks/useSetState";
+import useFetch from "@/components/hooks/useFetch";
 
-let pageIndex = 1;
+import HomeIcon from "@/pages/order/confirm/shop.svg";
+
+import { ORDER } from "@/utils/api";
 
 const Order = () => {
   const [current, setCurrent] = useState(0);
+  const [page, setPage] = useState(1);
+  const [state, setState] = useSetState({
+    isLoaded: true,
+    hasMore: false,
+    list: [
+      {
+        payId: 100,
+        shop: "lwgsh001测试专卖店",
+        shopId: 12,
+        orderId: 100,
+        orderSN: 5420000000010000,
+        orderTime: "2020-02-21 15:58:47",
+        autoCancelTime: "2020-02-21 16:58:47",
+        autoReceiveTime: null,
+        ordersRefundState: 0,
+        payAmount: 0.02,
+        isVirtual: 0,
+        shipSn: "",
+        shipCode: "",
+        sendTime: null,
+        type: "",
+        price: 0.02,
+        express: 0.01,
+        status: 10,
+        statusName: "待付款",
+        invoice: {},
+        goods: [
+          {
+            spuid: 92,
+            goodsId: 94,
+            ordersGoodsId: 1199,
+            title: "lwgsh001贵金属-养生银器-茶具01",
+            url:
+              "https://statictest.ccgold.cn/image/d2/a7/d2a7fbdcabec488f9edb80b8e1309d15.jpg",
+            type: "颜色：粉色",
+            price: 0.01,
+            count: 1
+          },
+          {
+            spuid: 92,
+            goodsId: 95,
+            ordersGoodsId: 1199,
+            title: "lwgsh001贵金属-养生银器-茶具01",
+            url:
+              "https://statictest.ccgold.cn/image/d2/a7/d2a7fbdcabec488f9edb80b8e1309d15.jpg",
+            type: "颜色：粉色",
+            price: 0.01,
+            count: 1
+          },
+          {
+            spuid: 92,
+            goodsId: 96,
+            ordersGoodsId: 1199,
+            title: "lwgsh001贵金属-养生银器-茶具01",
+            url:
+              "https://statictest.ccgold.cn/image/d2/a7/d2a7fbdcabec488f9edb80b8e1309d15.jpg",
+            type: "颜色：粉色",
+            price: 0.01,
+            count: 1
+          },
+          {
+            spuid: 92,
+            goodsId: 97,
+            ordersGoodsId: 1199,
+            title: "lwgsh001贵金属-养生银器-茶具01",
+            url:
+              "https://statictest.ccgold.cn/image/d2/a7/d2a7fbdcabec488f9edb80b8e1309d15.jpg",
+            type: "颜色：粉色",
+            price: 0.01,
+            count: 1
+          }
+        ],
+        address: {
+          name: "moren",
+          phone: "13399990000",
+          detail: "北京 北京市 东城区",
+          address: "mmmrrr"
+        }
+      },
+      {
+        payId: 99,
+        shop: "中钞贵金属",
+        shopId: 3,
+        orderId: 99,
+        orderSN: 5230000000009900,
+        orderTime: "2020-02-21 15:57:50",
+        autoCancelTime: "2020-02-21 16:57:50",
+        autoReceiveTime: null,
+        ordersRefundState: 0,
+        payAmount: 5,
+        isVirtual: 0,
+        shipSn: "",
+        shipCode: "",
+        sendTime: null,
+        type: "",
+        price: 5,
+        express: 0,
+        status: 10,
+        statusName: "待付款",
+        invoice: {},
+        goods: [
+          {
+            spuid: 108,
+            goodsId: 113,
+            ordersGoodsId: 1198,
+            title: "秒杀六号",
+            url:
+              "https://statictest.ccgold.cn/image/a3/60/a360786e9984ce666bb168f4c16277cc.jpg",
+            type: "",
+            price: 5,
+            count: 1
+          }
+        ],
+        address: {
+          name: "moren",
+          phone: "13399990000",
+          detail: "北京 北京市 东城区",
+          address: "mmmrrr"
+        }
+      }
+    ]
+  });
+
+  //   const { loading } = useFetch({
+  //     param: {
+  //       ...ORDER.list,
+  //       params: {
+  //         ordersState: ["all", "new", "pay", "send", "noeval", "cancel"][current], //全部订单
+  //         ordersType: db.EOrderTypes.real, // 实物订单
+  //         keyword: "" // 搜索关键词 订单号或商品
+  //       }
+  //     },
+  //     callback: (e: {
+  //       pageEntity;
+  //       ordersPayVoList: db.IOrderItem[];
+  //       [key: string]: any;
+  //     }) => {
+  //       let { hasMore } = e.pageEntity;
+  //       let list = db.convertOrderData(e.ordersPayVoList);
+  //       setState({
+  //         hasMore,
+  //         list,
+  //         isLoaded: page === 1
+  //       });
+  //     }
+  //   });
 
   // 更新cat信息
   const handleMenu = index => {
     setCurrent(index);
   };
 
-  const [state, setState] = useSetState({
-    isLoaded: false,
-    error: false,
-    hasMore: true,
-    isEmpty: false,
-    list: []
-  });
-
-  const getData = async (pIndex = pageIndex) => {
-    if (pIndex === 1) {
+  const getData = async (pageIndex = page) => {
+    if (pageIndex === 1) {
       setState({ isLoaded: false });
     }
     const {
@@ -34,21 +175,16 @@ const Order = () => {
       url: "https://cnodejs.org/api/v1/topics",
       data: {
         limit: 10,
-        page: pIndex
+        page: pageIndex
       }
     });
-    return { list: data, hasMore: true, isLoaded: pIndex === 1 };
+    return { list: data, hasMore: true, isLoaded: pageIndex === 1 };
   };
-
-  useEffect(() => {
-    getData().then(res => {
-      setState(res);
-    });
-  }, []);
 
   const onScrollToLower = async fn => {
     const { list } = state;
-    const { list: newList, hasMore } = await getData(++pageIndex);
+    const { list: newList, hasMore } = await getData(page + 1);
+    setPage(page + 1);
     setState({
       list: list.concat(newList),
       hasMore
@@ -65,33 +201,57 @@ const Order = () => {
         lazy=".lazy-view"
         isLoaded={state.isLoaded}
         hasMore={state.hasMore}
-        style={{ height: "calc(100% - 40px)" }}
+        style={{ height: "calc(100% - 40px)", background: "#f8f8f8" }}
         onScrollToLower={onScrollToLower}
         onPullDownRefresh={onScrollToLower}
-        className="order_detail at-list"
+        className="order_detail"
       >
-        {state.list.map(item => {
+        {state.list.map(order => {
           return (
-            <View className="at-list__item at-list__item--thumb" key={item.id}>
-              <View className="at-list__item-container">
-                <View className="at-list__item-thumb">
-                  <Image
-                    mode="scaleToFill"
-                    className="item-thumb"
-                    src="https://img12.360buyimg.com/jdphoto/s72x72_jfs/t6160/14/2008729947/2754/7d512a86/595c3aeeNa89ddf71.png"
-                  />
+            <View className="at-list" key={order.payId}>
+              <View className="header">
+                <View className="shop">
+                  <Image src={HomeIcon} className="icon" />
+                  <Text className="title">{order.shop}</Text>
                 </View>
+                <View className="status">{order.statusName}</View>
+              </View>
 
-                <View className="at-list__item-content item-content">
-                  <View className="item-content__info">
-                    <View className="item-content__info-title">
-                      {item.title}
+              {order.goods.map(goodsItem => (
+                <View
+                  className="at-list__item at-list__item--thumb"
+                  key={goodsItem.goodsId}
+                >
+                  <View className="at-list__item-container">
+                    <View className="at-list__item-thumb">
+                      <Image
+                        mode="scaleToFill"
+                        className="item-thumb"
+                        src={goodsItem.url}
+                      />
                     </View>
-                    <View className="item-content__info-subtitle">
-                      {item.title.slice(0, 8)}
+
+                    <View className="at-list__item-content item-content">
+                      <View className="item-content__info">
+                        <View className="item-content__info-title spaceBetween">
+                          <Text>{goodsItem.title}</Text>
+                          <Text>￥{goodsItem.price}</Text>
+                        </View>
+                        <View className="item-content__info-subtitle spaceBetween">
+                          <Text>{goodsItem.type}</Text>
+                          <Text>× {goodsItem.count}</Text>
+                        </View>
+                      </View>
                     </View>
                   </View>
                 </View>
+              ))}
+
+              <View className="footer">
+                <Text>
+                  运费：￥{order.express}，共 {order.goods.length} 件商品
+                </Text>
+                <Text className="payAmount">实付：￥{order.payAmount}</Text>
               </View>
             </View>
           );
