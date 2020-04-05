af0befe0e83eae6948a3357090c1cc625ffc2ba5
realeve <realeve@qq.com>
2020-04-05 12:55:27 +0800

    订单确认页样式优化

--- src/pages/order/confirm/index.tsx
+++ src/pages/order/confirm/index.tsx
@@ -53,13 +53,11 @@ const OrderConfirm = ({ currentAddress }) => {
     },
   });
 
-  // console.log(address);
-
   useEffect(() => {
     if (!currentAddress.address_id) {
       return;
     }
-    // setData(currentAddress);
+    setData(currentAddress);
   }, [JSON.stringify(currentAddress)]);
 
   const [goodsList, setGoodsList] = useState<cartDb.IBuyGoodsItemVoList[]>([]);
@@ -91,33 +89,51 @@ const OrderConfirm = ({ currentAddress }) => {
       });
   }, []);
 
+  console.log(goodsList);
+
   return (
     <View className="order_confirm">
       <AddressPanel data={address} />
-      {goodsList.map((goodsItem: cartDb.IBuyGoodsItemVoList) => (
-        <CCardLite className="goodslist" key={goodsItem.commonId}>
+      {goodsList.map((item: cartDb.IBuyGoodsItemVoList) => (
+        <CCardLite className="goodslist" key={item.commonId}>
           <View className="shop">
             <Image src={HomeIcon} className="icon" />
-            <Text className="title">{goodsItem.storeName}</Text>
+            <Text className="title">{item.storeName}</Text>
           </View>
 
           <View className="item">
-            <Image src={goodsItem.spuImageSrc} className="img" />
+            <View className="main">
+              <Image src={item.spuImageSrc} className="img" />
+              <View className="detail">
+                <View className="main">
+                  <Text className="goods_name">{item.goodsName} aasd</Text>
+                  <CPrice retail={item.goodsPrice} />
+                </View>
 
-            <View className="detail">
-              <View className="main">
-                <Text className="goods_name">{goodsItem.goodsName} aasd</Text>
-                <CPrice retail={goodsItem.goodsPrice} />
+                <View className="sub">
+                  <Text>{item.goodsFullSpecs}</Text>
+                  <Text>
+                    x {item.spuBuyNum}
+                    {item.unitName}
+                  </Text>
+                </View>
               </View>
-
-              <View className="sub">
-                <Text>{goodsItem.goodsFullSpecs}</Text>
-                <Text>x {goodsItem.spuBuyNum}</Text>
+            </View>
+            <View className="express">
+              <View>配送方式</View>
+              <View>
+                快递:
+                {item.goodsFreight == 0 ? "免邮" : "￥" + item.goodsFreight}
+                <Text className="at-icon item-extra__icon-arrow at-icon-chevron-right" />
               </View>
             </View>
           </View>
         </CCardLite>
       ))}
+      <View className="invoice">
+        <View>发票</View>
+        <View>{invoice.title}</View>
+      </View>
     </View>
   );
 };
