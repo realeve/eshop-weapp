4f309a1b52e23e17dca6e538658a883683d879d7
musicoooool <musicoooool@gmail.com>
2020-05-06 13:19:31 +0800

    增加订单超时付款限制

--- src/pages/user/order/components/Pay/index.tsx
+++ src/pages/user/order/components/Pay/index.tsx
@@ -1,16 +1,31 @@
-import Taro from "@tarojs/taro";
+import Taro, { useState, useEffect } from "@tarojs/taro";
 import { CButton } from "@/components/";
 import { getMpPrepayId } from "@/utils/cartDb";
 import { pay } from "@/utils/login";
+import moment from "dayjs";
 
-export default ({ payId, onRefresh }) => {
+export default ({ payId, onRefresh, autoCancelTime }) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  useEffect(() => {
+    loading
+      ? Taro.showLoading({ title: "支付中", mask: true })
+      : Taro.hideLoading();
+  }, [loading]);
   return (
     <CButton
       theme="yellow"
       size="small"
       onClick={async () => {
-        let res = await getMpPrepayId({ payId, predepositPay: 0 });
-        pay(res, onRefresh);
+        if (moment().isAfter(autoCancelTime)) {
+          Taro.showModal({
+            title: "提示",
+            content: "订单付款超时",
+            showCancel: false
+          });
+          return;
+        }
+        setLoading(true);
+        pay(payId, onRefresh).finally(() => setLoading(false));
       }}
       round={false}
       style="margin-left:12px"
