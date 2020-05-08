c732e62754da602995fdeb72b7b182f7f0a12c45
realeve <realeve@qq.com>
2020-05-09 00:20:45 +0800

    逻辑优化

--- src/components/CityPicker/index.tsx
+++ src/components/CityPicker/index.tsx
@@ -1,45 +1,36 @@
-import Taro, { useState, useEffect } from "@tarojs/taro";
+import Taro, { useState } from "@tarojs/taro";
 import { View, Text, Picker } from "@tarojs/components";
-import useSetState from "@/components/hooks/useSetState";
 import useFetch from "../hooks/useFetch";
 import "./index.scss";
 import { cityUrl } from "@/utils/setting";
 import * as R from "ramda";
 
+const getInitArea = data => {
+  let _city = data[0].sub.map(item => item.areaName),
+    _prov = data.map(item => item.areaName),
+    _area = data[0].sub[0].sub.map(item => item.areaName);
+  return [_prov, _city, _area];
+};
+
 const PagePicker = props => {
+  const [state, setState] = useState([[], [], []]);
+
   const { data, loading } = useFetch({
     param: { url: cityUrl },
-    callback: e => e.datas.areaList
+    callback: e => {
+      let dist = e.datas.areaList;
+      let selector = getInitArea(dist);
+      setState(selector);
+      return dist;
+    }
   });
 
   let [idxProv, setIdxProv] = useState(0);
 
-  const [state, setState] = useSetState({
-    selector: [[], [], []],
-    selectorChecked: props.value
-  });
-
-  useEffect(() => {
-    setState({ selectorChecked: props.value });
-  }, [props.value]);
-
-  useEffect(() => {
-    if (!data) {
-      return;
-    }
-    let _city = data[0].sub.map(item => item.areaName),
-      _prov = data.map(item => item.areaName),
-      _area = data[0].sub[0].sub.map(item => item.areaName);
-
-    setState({
-      selector: [_prov, _city, _area]
-    });
-  }, [data]);
-
   const onChange = e => {
     let division = props.Division || " ";
 
-    let [prov, city, area = 0] = e.detail.value;
+    let [prov = 0, city = 0, area = 0] = e.detail.value;
 
     let curAddress =
       data[prov].areaName +
@@ -48,9 +39,6 @@ const PagePicker = props => {
       division +
       data[prov].sub[city].sub[area].areaName;
 
-    setState({
-      selectorChecked: curAddress
-    });
     props.onChange && props.onChange(curAddress);
   };
 
@@ -59,7 +47,8 @@ const PagePicker = props => {
     if (!indexVal) {
       return;
     }
-    let prevState = R.clone(state.selector);
+
+    let prevState = R.clone(state);
 
     let _city = [];
     let _area = [];
@@ -71,18 +60,14 @@ const PagePicker = props => {
       _city = provData.sub.map(item => item.areaName);
       _area = provData.sub[0].sub.map(item => item.areaName);
 
-      setState({
-        selector: [prevState[0], _city, _area]
-      });
+      setState([prevState[0], _city, _area]);
     } else if (indexVal.column == 1) {
       let provData = data[idxProv];
       _city = provData.sub.map(item => item.areaName);
 
       _area = provData.sub[idx].sub.map(item => item.areaName);
 
-      setState({
-        selector: [prevState[0], prevState[1], _area]
-      });
+      setState([prevState[0], prevState[1], _area]);
     }
   };
 
@@ -90,13 +75,13 @@ const PagePicker = props => {
     <View className="city_picker">
       <Picker
         mode="multiSelector"
-        range={state.selector}
+        range={state}
         onColumnChange={onColumnChange}
         onChange={onChange}
       >
         <View className="wrap">
           <View className="title">{props.title || "地址"}</View>
-          <Text>{state.selectorChecked}</Text>
+          <Text>{props.value}</Text>
         </View>
       </Picker>
     </View>
