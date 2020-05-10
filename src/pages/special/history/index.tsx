1414055992005bbe3b1a43449192dee920c0e446
realeve <realeve@qq.com>
2020-05-10 13:11:28 +0800

    历史预约样式对接基本完成

--- src/pages/special/history/index.tsx
+++ src/pages/special/history/index.tsx
@@ -1,17 +1,12 @@
-import Taro, { useState, useEffect } from "@tarojs/taro";
-import { View, Text } from "@tarojs/components";
+import Taro, { useState } from "@tarojs/taro";
+import { View } from "@tarojs/components";
 import ListView from "taro-listview";
 import { SPECIAL_GOODS } from "@/utils/api";
 import useFetch from "@/components/hooks/useFetch";
 import useSetState from "@/components/hooks/useSetState";
-import { CEmpty, CPrice, CButton } from "@/components";
+import { CEmpty } from "@/components";
 import { historyHandler } from "./lib";
-import "./index.scss";
-import * as lib from "@/utils/lib";
-import { AtIcon } from "taro-ui";
-// import { CardCountdown } from "@/pages/special/history/index";
-import { CImgCard } from "@/pages/special/components/CImgCard";
-import CountTime from "@/pages/user/order/components/CountTime";
+import PreorderItem from "./preorderItem";
 
 interface IOrderItem {
   activityId: number; // 主KEY
@@ -90,93 +85,7 @@ const History = () => {
         onPullDownRefresh={fn => onRefresh(fn)}
       >
         {state.list.map((data: IOrderItem) => (
-          <View className="detail" key={data.activityId}>
-            <View
-              className="goods"
-              onClick={() => {
-                lib.jump(`/special/page/${data.activityId}`);
-              }}
-            >
-              <CImgCard
-                data={{
-                  href: `/special/page/${data.activityId}`,
-                  img: data.goodsItem.imageSrc
-                }}
-                style={{ margin: "10px 0", background: "#fff" }}
-              />
-              {/* <View className="mask}></View>
-                  <img src={goodsItem.imageSrc} alt="" /> */}
-            </View>
-            <View className="goodsInfo" key={data.goodsItem.goodsId}>
-              <View className="goodsMain">
-                <View className="goodsTitle">
-                  {data.promo}
-                  {data.goodsName}
-                </View>
-                <View className="thumbnail">
-                  <p>{data.jingle}</p>
-                </View>
-                <View className="title">
-                  <View className="info">
-                    <View className="name">预约价</View>
-                    <View className="value">
-                      <CPrice retail={data.goodsPrice} className="price" />
-                    </View>
-                  </View>
-                  <View className="info">
-                    <View className="name">预约数量</View>
-                    <View className="value">{data.issueQuantity}</View>
-                  </View>
-                  <View className="info">
-                    <View className="name">预约开始时间</View>
-                    <View className="value">{data.beginTime}</View>
-                  </View>
-                  <View className="info">
-                    <View className="name">预约结束时间</View>
-                    <View className="value">{data.endTime}</View>
-                  </View>
-                  <View className="info">
-                    <View className="name">抽签时间</View>
-                    <View className="value">{data.drawTime}</View>
-                  </View>
-                  {/* <View className="status}>
-                        <a className="desc}>{data.statusName}</a>
-                        <CountTime time={remainTime} />
-                      </View> */}
-                </View>
-              </View>
-              <View className="type">
-                {data.showCountDown && (
-                  <View>
-                    {/* <AtIcon className="icon" value="clock" />
-                    <span className="limitTitle">还有</span> */}
-                    <View className="countime">
-                      <CountTime time={data.remainTime} />
-                      {/* <CardCountdown time={data.remainTime} type={"small"} /> */}
-                    </View>
-                    <Text className="limitTitle">{data.operation}</Text>
-                  </View>
-                )}
-                <View className="button">
-                  <CButton
-                    wide
-                    theme="yellowGardiant"
-                    style={{
-                      width: 120,
-                      height: 32,
-                      lineHeight: 1,
-                      margin: 11
-                    }}
-                    onClick={() => {
-                      lib.jump(`/special/page/${data.activityId}`);
-                    }}
-                  >
-                    {data.buttonTitle}
-                  </CButton>
-                </View>
-              </View>
-            </View>
-          </View>
+          <PreorderItem key={data.activityId} data={data} />
         ))}
       </ListView>
     </View>
---
+++ src/pages/special/history/preorderItem.tsx
@@ -0,0 +1,82 @@
+import Taro from "@tarojs/taro";
+import { View, Text, Image } from "@tarojs/components";
+import { CPrice, CButton } from "@/components";
+import { jump } from "@/utils/lib";
+import CountTime from "@/pages/user/order/components/CountTime";
+import "./preorderItem.scss";
+
+export default ({ data }) => {
+  const url = `/pages/special/index?id=${data.activityId}`;
+
+  return (
+    <View className="item">
+      <View className="detail">
+        <View
+          className="goods"
+          onClick={() => {
+            jump(url);
+          }}
+        >
+          <Image src={data.goodsItem.imageSrc} className="img" />
+        </View>
+        <View className="goodsInfo" key={data.goodsItem.goodsId}>
+          <View className="goodsMain">
+            <View className="goodsTitle">
+              {data.promo}
+              {data.goodsName}
+            </View>
+            <View className="title">
+              <View className="info">
+                <View className="name">预约价</View>
+                <View className="value">
+                  <CPrice retail={data.goodsPrice} />
+                </View>
+              </View>
+              <View className="info">
+                <View className="name">预约数量</View>
+                <View className="value">{data.issueQuantity}</View>
+              </View>
+              <View className="info">
+                <View className="name">预约开始</View>
+                <View className="value">{data.beginTime}</View>
+              </View>
+              <View className="info">
+                <View className="name">预约结束</View>
+                <View className="value">{data.endTime}</View>
+              </View>
+              <View className="info">
+                <View className="name">抽签时间</View>
+                <View className="value">{data.drawTime}</View>
+              </View>
+            </View>
+          </View>
+        </View>
+      </View>
+      <View className="description">{data.jingle}</View>
+
+      <View className="type">
+        <View className="time">
+          {data.showCountDown && (
+            <View>
+              <CountTime time={data.remainTime} />
+              <Text className="limitTitle">{data.operation}</Text>
+            </View>
+          )}
+        </View>
+
+        <View style={{ width: "160px" }}>
+          <CButton
+            theme="gardient"
+            round={false}
+            size="small"
+            onClick={() => {
+              jump(url);
+            }}
+          >
+            {data.buttonTitle}
+          </CButton>
+        </View>
+      </View>
+    </View>
+  );
+};
